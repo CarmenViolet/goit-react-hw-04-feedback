9560e0d4d1b70d2a7b1a11c687c92958d41abd5b
CarmenViolet <margaretholmes200@gmail.com>
2022-10-13 17:40:49 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 const FeedbackOptions = ({options, onLeaveFeedback}) => {
     return (
         <>
-            <button className={css.button__option} name="good" value={options.good} onClick={onLeaveFeedback}>Good</button>
-            <button className={css.button__option} name="neutral" value={options.neutral} onClick={onLeaveFeedback}>Neutral</button>
-            <button className={css.button__option} name="bad" value={options.bad} onClick={onLeaveFeedback}>Bad</button>
+            <button type="button" className={css.button__option} name="good" value={options.good} onClick={onLeaveFeedback}>Good</button>
+            <button type="button" className={css.button__option} name="neutral" value={options.neutral} onClick={onLeaveFeedback}>Neutral</button>
+            <button type="button" className={css.button__option} name="bad" value={options.bad} onClick={onLeaveFeedback}>Bad</button>
         </>
     )
 }
